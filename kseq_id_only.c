f4e239853ef6ccbf9e1401df02eb0344ef1d8b85
Jan van Haarst <jan.vanhaarst@wur.nl>
2016-06-10 21:54:30 +0200

    Print only id of sequences

---
+++ kseq_id_only.c
@@ -0,0 +1,17 @@
+#include <zlib.h>
+#include <stdio.h>
+#include "kseq.h"
+KSEQ_INIT(gzFile, gzread)
+
+int main(void)
+{
+	gzFile fp;
+	kseq_t *seq;
+	fp = gzdopen(fileno(stdin), "r");
+	seq = kseq_init(fp);
+	while (kseq_read(seq) >= 0)
+		printf("%s\n", seq->name.s);
+	kseq_destroy(seq);
+	gzclose(fp);
+	return 0;
+}
