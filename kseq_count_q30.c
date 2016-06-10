a8106e7cb41588affd2153034e556d75eccb1cb7
Jan van Haarst <jan.vanhaarst@wur.nl>
2016-06-10 22:17:47 +0200

    Print statistics, including bases better than Q30

---
+++ kseq_count_q30.c
@@ -0,0 +1,32 @@
+#include <zlib.h>
+#include <stdio.h>
+#include "kseq.h"
+KSEQ_INIT(gzFile, gzread)
+
+int main(void)
+{
+    gzFile fp;
+    kseq_t *seq;
+    unsigned long long n = 0, slen = 0, q30 = 0, k=0;
+    double average;
+    int offset = 33, qual = 0;
+    fp = gzdopen(fileno(stdin), "r");
+    seq = kseq_init(fp);
+    while (kseq_read(seq) >= 0){
+        for(k = 0;k < seq->seq.l;++k){ 
+            char qualchar = seq->qual.s[k];
+            qual = (int) qualchar - offset;
+            if (qual >= 30){
+                ++q30;
+            }
+        };
+        ++n, slen += seq->seq.l;
+    }
+    average = (double) slen / n;
+
+    printf("Number of sequences: %llu\tAverage length: %4.3f\tTotal number of bases: %llu\tNumber of bases >= Q30: %llu\n", n, average, slen, q30);
+
+    kseq_destroy(seq);
+    gzclose(fp);
+    return 0;
+}
