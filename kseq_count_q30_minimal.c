8f5b85a82c0544c195ad3458adc64f43d98f57c1
Jan van Haarst <jan.vanhaarst@wur.nl>
2017-03-23 16:56:09 +0100

    Added counter without comments

---
+++ kseq_count_q30_minimal.c
@@ -0,0 +1,33 @@
+#include <zlib.h>
+#include <stdio.h>
+#include "kseq.h"
+KSEQ_INIT(gzFile, gzread)
+
+int main(void)
+{
+    gzFile fp;
+    kseq_t *seq;
+    unsigned long long n = 0, slen = 0, q30 = 0, k=0;
+    double average, percentage_q30;
+    int offset = 33, qual = 0;
+    fp = gzdopen(fileno(stdin), "r");
+    seq = kseq_init(fp);
+    while (kseq_read(seq) >= 0){
+        for(k = 0;k < seq->seq.l;++k){ 
+            char qualchar = seq->qual.s[k];
+            qual = (int) qualchar - offset;
+            if (qual >= 30){
+                ++q30;
+            }
+        };
+        ++n, slen += seq->seq.l;
+    }
+    average = (double) slen / n;
+    percentage_q30= ((double) q30 / slen)*100;
+
+    printf("%llu\t%4.3f\t%llu\t%llu\t(%4.3f%%)\n", n, average, slen, q30, percentage_q30);
+
+    kseq_destroy(seq);
+    gzclose(fp);
+    return 0;
+}
